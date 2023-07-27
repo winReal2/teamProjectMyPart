613483fcba7fff88c660651869e8510e7970e6a3
winReal2 <tmdwiswkd@hanmail.net>
2023-07-27 18:05:52 +0900

    컨트롤러 추가

--- teamProject/src/main/java/com/peco/member_Controller/MemberController.java
+++ teamProject/src/main/java/com/peco/member_Controller/MemberController.java
@@ -1,8 +1,19 @@
 package com.peco.member_Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.peco.member_Service.MemberService;
 
 @Controller
 public class MemberController {
 
+	@Autowired
+	MemberService service;
+	
+	@GetMapping("/MyPage")
+	public String update() {
+		return "redirect:/MyPage";
+	}
 }
